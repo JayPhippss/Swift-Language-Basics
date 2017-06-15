fd1bca43f226ef067207d6458c325671a76a45d3
JayPhippss <PhippsJaylin@gmail.com>
2017-06-15 18:59:24 -0500

    First commit to project

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Jaylin Phipps on 6/15/17.
+//  Copyright © 2017 Jaylin Phipps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
